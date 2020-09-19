0c687e37f23d5e9bf92a36be62399cfd0ddf78d0
Brian Liu <brian.b.liu@gmail.com>
2020-09-19 16:22:16 -0600

    Add test it exists to Enigma class.

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,5 +5,11 @@ require 'mocha/minitest'
 require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+
+  end
 
 end
