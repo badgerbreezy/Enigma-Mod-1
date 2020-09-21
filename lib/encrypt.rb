7811bea67c2ea627b6466cc12a4c002956202761
Brian Liu <brian.b.liu@gmail.com>
2020-09-21 17:37:51 -0600

    Add ability for encrypt class to generate random key.

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,8 +1,8 @@
 class Encrypt
   attr_reader :characters, :date, :key
 
-  def initialize(key)
-    @key = key
+  def initialize
+    @key = generate_key
     @date = Date.today.strftime("%d%m%y")
     @characters = CharactersList.new.characters
   end
@@ -11,8 +11,9 @@ class Encrypt
     rand(9 ** 5).to_s.rjust(5, '0')
   end
 
-  def key_shift(key)
+  def key_shift
     key_array = key.split("")
+    # require 'Pry';binding.pry
     sum = key_array.map do |number|
       number.to_i
     end
@@ -32,48 +33,48 @@ class Encrypt
     end
   end
 
-  def total_shift(key)
+  def total_shift
     key_date = []
-    key_date << key_shift(key)
+    key_date << key_shift
     key_date << date_shift
     key_date.transpose.map do |sub_array|
       sub_array.sum
     end
   end
 
-  def a_rotation(letter, key)
-    a_rotation = @characters.rotate(total_shift(key)[0])
+  def a_rotation(letter)
+    a_rotation = @characters.rotate(total_shift[0])
     Hash[characters.zip(a_rotation)][letter]
   end
 
 
-  def b_rotation(letter, key)
-    b_rotation = @characters.rotate(total_shift(key)[1])
+  def b_rotation(letter)
+    b_rotation = @characters.rotate(total_shift[1])
     Hash[characters.zip(b_rotation)][letter]
   end
 
 
-  def c_rotation(letter, key)
-    c_rotation = @characters.rotate(total_shift(key)[2])
+  def c_rotation(letter)
+    c_rotation = @characters.rotate(total_shift[2])
     Hash[characters.zip(c_rotation)][letter]
   end
 
 
-  def d_rotation(letter, key)
-    d_rotation = @characters.rotate(total_shift(key)[3])
+  def d_rotation(letter)
+    d_rotation = @characters.rotate(total_shift[3])
     Hash[characters.zip(d_rotation)][letter]
   end
 
-  def encrypt(message, key)
+  def encrypt(message)
     message_array = message.downcase.split("").each_slice(4).to_a
     encrypted_array = []
     message_array.each do |sub_array|
-      encrypted_array << a_rotation(sub_array[0], key)
-      encrypted_array << b_rotation(sub_array[1], key)
-      encrypted_array << c_rotation(sub_array[2], key)
-      encrypted_array << d_rotation(sub_array[3], key)
+      encrypted_array << a_rotation(sub_array[0])
+      encrypted_array << b_rotation(sub_array[1])
+      encrypted_array << c_rotation(sub_array[2])
+      encrypted_array << d_rotation(sub_array[3])
     end
     encrypted = encrypted_array.join
-    encryption_hash = {encryption: encrypted, key: key}
+    encryption_hash = {encryption: encrypted, key: key, date: date}
   end
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -8,59 +8,62 @@ require './lib/encrypt'
 require './lib/characters_list'
 
 class EncryptTest < Minitest::Test
-  def test_it_exists
+  def test_it_exists_and_has_attributes
     Date.stubs(:today).returns(Date.new(1995,8,4))
-    encrypt = Encrypt.new("02715")
+    encrypt = Encrypt.new
+    encrypt.stubs(:key).returns('02715')
+
 
     assert_instance_of Encrypt, encrypt
-    assert_equal "040895", enigma.date
+    assert_equal "040895", encrypt.date
+    assert_equal "02715", encrypt.key
+
   end
 
   def test_it_can_generate_key
-    encrypt = Encrypt.new("02715")
+    encrypt = Encrypt.new
     encrypt.stubs(:generate_key).returns('02715')
 
     assert_equal "02715", encrypt.generate_key
   end
 
   def test_it_can_shift_key
-    encrypt = Encrypt.new("02715")
+    encrypt = Encrypt.new
+    encrypt.stubs(:key).returns('02715')
+    assert_equal "02715", encrypt.key
 
-    assert_equal [02, 27, 71, 15], encrypt.key_shift("02715")
+    assert_equal [02, 27, 71, 15], encrypt.key_shift
   end
 
   def test_it_can_generate_date # test more later
-    encrypt = Encrypt.new("02715")
+    encrypt = Encrypt.new
     Date.stubs(:today).returns(Date.new(1995,8,4))
+    encrypt.stubs(:key).returns('02715')
     assert_equal [6, 4, 0, 0], encrypt.date_shift
   end
 
   def test_total_shift
     Date.stubs(:today).returns(Date.new(1995,8,4))
-    encrypt = Encrypt.new("02715")
-    assert_equal [3, 27, 73, 20], encrypt.total_shift("02715")
-    assert_equal [3, 27, 73, 20], encrypt.total_shift("02715")
-  end
-
-  def test_encryption_shift
-    Date.stubs(:today).returns(Date.new(1995,8,4))
-    encrypt = Encrypt.new("02715")
-
-    assert_equal [3, 27, 73, 20], encrypt.total_shift("02715")
+    encrypt = Encrypt.new
+    encrypt.stubs(:key).returns('02715')
+    assert_equal [3, 27, 73, 20], encrypt.total_shift
+    assert_equal [3, 27, 73, 20], encrypt.total_shift
   end
 
   def test_rotate_letter
     Date.stubs(:today).returns(Date.new(1995,8,4))
-    encrypt = Encrypt.new("02715")
-    assert_equal "k", encrypt.a_rotation("h", "02715")
-    assert_equal "e", encrypt.b_rotation("e", "02715")
-    assert_equal "d", encrypt.c_rotation("l", "02715")
-    assert_equal "e", encrypt.d_rotation("l", "02715")
+    encrypt = Encrypt.new
+    encrypt.stubs(:key).returns('02715')
+    assert_equal "k", encrypt.a_rotation("h")
+    assert_equal "e", encrypt.b_rotation("e")
+    assert_equal "d", encrypt.c_rotation("l")
+    assert_equal "e", encrypt.d_rotation("l")
   end
 
   def test_it_can_encrypt
     Date.stubs(:today).returns(Date.new(1995,8,4))
-    encrypt = Encrypt.new("02715")
-    assert_equal ({:encryption=>"keder ohulw", :key=>"02715"}), encrypt.encrypt("hello world", "02715")
+    encrypt = Encrypt.new
+    encrypt.stubs(:key).returns('02715')
+    assert_equal ({:encryption =>"keder ohulw", :key => "02715", :date=> "040895"}), encrypt.encrypt("hello world")
   end
 end
