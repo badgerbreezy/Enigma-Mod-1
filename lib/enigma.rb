db01f96cf1eea6af38f231a75d545bebfb6a323f
Brian Liu <brian.b.liu@gmail.com>
2020-09-21 17:55:10 -0600

    Revise enigma tests for random key.

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,18 +3,15 @@ class Enigma
   def initialize
     @message = message
     @key = key
-
     @encrypt = Encrypt.new(key)
     @decrypt = Decrypt.new(key)
   end
 
   def encrypt(message, key)
-
     @encrypt.encrypt(message, key)
   end
 
   def decrypt(message, key)
-
     @decrypt.decrypt(message, key)
   end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,7 +11,6 @@ require './lib/characters_list'
 class EnigmaTest < Minitest::Test
   def test_it_exists_and_has_attributes
     enigma = Enigma.new
-    encrypt = Encrypt.new("02715")
 
     assert_instance_of Enigma, enigma
   end
@@ -19,16 +18,21 @@ class EnigmaTest < Minitest::Test
   def test_it_can_encrypt_and_decrypt
     Date.stubs(:today).returns(Date.new(1995,8,4))
     enigma = Enigma.new
-    encrypt = Encrypt.new("02715")
-    decrypt = Decrypt.new("02715")
+    encrypt = Encrypt.new
+    decrypt = Decrypt.new
 
-    assert_equal ({:encryption=>"keder ohulw", :key=>"02715"}), enigma.encrypt("hello world", "02715")
-    encrypted = enigma.encrypt("hello world", "02715")
+    assert_equal ({
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      }), enigma.encrypt("hello world")
+    encrypted = enigma.encrypt("hello world")
 
     assert_equal ({
         decryption: "hello world",
-        key: "02715"
-      }), enigma.decrypt(encrypted[:encryption], "02715")
+        key: "02715",
+        date: "040895"
+        }), enigma.decrypt(encrypted[:encryption])
   end
 
 end
